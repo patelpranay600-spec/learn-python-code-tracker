80c2f9bbb3d3b5f1a00338dcdd3794cc06268d29
patelpranay600-spec <patelpranay600@gmail.com>
2025-09-04 08:25:52 +0530

    Add files via upload

---
+++ # calculator.py
@@ -0,0 +1,17 @@
+# calculator
+a=int(input("first no"))
+b=int(input("second no"))
+opr=input("multi->*,divid-press enter,add->+,subs'->- ")
+# calculTIONS 
+
+if(opr=="*"):
+    print("the product is",a*b)
+if(opr=="+"):
+    print("the sum is",a+b)    
+if(opr=="-"):
+    print("the diffrence is",a-b)
+
+else:
+    print("the division qotient is",a/b)
+
+
