fd31fbc2de9cded3ac6a80882176282084dc9f55
patelpranay600-spec <patelpranay600@gmail.com>
2025-09-04 17:51:37 +0530

    Add files via upload

---
+++ rockpaper.py
@@ -0,0 +1,55 @@
+# rock paper 
+print("for rock-R,paper-P,scissor-S")
+pl1=input("choice of player 1")
+pl2=input("choice of player 2")
+
+if(pl1=="R"):
+    if(pl2=="P"):
+        print("pl2 won the game")
+    elif(pl1==pl2):
+        print("invalid") 
+         
+
+if(pl1=="S"):
+    if(pl2=="R"):
+        print("pl2 won the game")
+
+    elif(pl1==pl2):
+        print("invalid")  
+
+    
+if(pl1=="P"):
+    if(pl2=="S"):
+        print("pl2 won the game")
+
+    elif(pl1==pl2):
+        print("invalid")  
+
+
+
+
+
+else:
+    print("pl1 won the game")  
+
+
+
+
+
+
+
+               
+
+
+
+    
+
+
+
+
+
+
+
+
+
+    
