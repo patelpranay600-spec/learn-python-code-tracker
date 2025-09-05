72139011ae199881fbfeb40553bd7b16970a1053
patelpranay600-spec <patelpranay600@gmail.com>
2025-09-05 09:26:51 +0530

    Add files via upload

---
+++ a program to reverse the sentence.py
@@ -0,0 +1,17 @@
+# a program to reverse the sentence
+st1=input("the sentence to be reversed--->")
+teststring=st1
+result=teststring.split()
+print("orignal setence",result)
+my_list=result
+my_list.reverse()
+print("",my_list)
+
+
+
+
+
+
+
+
+
